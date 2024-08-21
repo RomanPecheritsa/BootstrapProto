13bd254bec34df246b75c352ce88dd4a13e88bbe
pecheritsa <pecheritsa.roman@gmail.com>
2024-08-21 22:37:32 +0300

    add main app module

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route('/<path:path>')
+def catch_all(path):
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run()
